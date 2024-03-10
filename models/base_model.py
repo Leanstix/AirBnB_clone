deb3aed30de99936ee2e98ec0ff40cc5e6b39a96
Leanstix <leanstixx@gmail.com>
2024-03-10 15:59:31 +0100

    ready

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+"""
+models/base_model.py
+Module containing the base model class
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+        Returns a string representation of of the BaseModel Instance
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel Instance
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
