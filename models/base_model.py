b33517be6eaefcf85ba76d68a3ed07ff7d2d2c2c
Leanstix <leanstixx@gmail.com>
2024-03-11 00:10:47 +0100

    third

--- models/base_model.py
+++ models/base_model.py
@@ -6,13 +6,28 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
+
+        If kwargs is not empty:
+        - Each key of this dictionary is an attribute name (excluding __class__).
+        - Each value of This dictionary is the value of the respective atrribute.
+        - converts created_at and uodated_at from string to datetime objects
+
+        otherwise:
+        - creates id and created_at as done previously (new instance).
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
